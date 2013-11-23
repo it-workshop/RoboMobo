af5ba442a6d6329afd70de21338351bf650d2386
Loredan <loredan725@gmail.com>
2013-11-23 11:50:09 +0400

    Базовая структура

---
+++ src/GameActivity.java
@@ -0,0 +1,17 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: loredan
+ * Date: 23.11.13
+ * Time: 11:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/Map.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: loredan
+ * Date: 23.11.13
+ * Time: 11:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Map
+{
+}
